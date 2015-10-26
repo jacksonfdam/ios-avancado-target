4459d72f10993ee399e0d42b1afefa829c81a688
Aluno <aluno@SL04TTDESK03.local>
2015-10-26 19:02:55 -0200

    First Import

---
+++ exercicio23/exercicio23/NoteListViewController.h
@@ -0,0 +1,15 @@
+//
+//  NoteListViewController.h
+//  exercicio23
+//
+//  Created by Targettrust Treinamentos on 12/15/14.
+//  Copyright (c) 2015 Targettrust Treinamentos. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface NoteListViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *notes;
+@end
